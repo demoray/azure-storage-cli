df57d7e53cf58dae9d901172170f65317667a9cf
Brian Caswell <bcaswell@gmail.com>
2024-01-03 15:45:43 -0500

    save the resulting EXEs (#44)

--- eng/build.sh
+++ eng/build.sh
@@ -1,16 +1,22 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 set -uvex -o pipefail
+
+BUILD_TARGET=${1:-""}
 
 cd $(dirname ${BASH_SOURCE[0]})/../
 
 which typos || cargo install typos-cli
 
 BUILD_COMMON="--locked --release"
+if [ x"${BUILD_TARGET}" != x"" ]; then
+    BUILD_COMMON="${BUILD_COMMON} --target ${BUILD_TARGET}"
+fi
 
 cargo clippy ${BUILD_COMMON} --all-targets --all-features -- -D warnings -D clippy::pedantic -A clippy::missing_errors_doc
 cargo clippy ${BUILD_COMMON} --tests --all-targets --all-features -- -D warnings
 cargo fmt --check
 cargo build ${BUILD_COMMON}
 cargo test ${BUILD_COMMON}
-cargo run ${BUILD_COMMON} -- --account EMPTY readme --check
+cargo run ${BUILD_COMMON} -- --account EMPTY readme > README.md
+git diff --exit-code README.md
