a6dc444fee7f948064799b1990c4d1b4e7a1a70a
Brian Caswell <bcaswell@microsoft.com>
2024-01-03 15:08:18 -0500

    pass in the target as appropriate

--- eng/build.sh
+++ eng/build.sh
@@ -2,11 +2,16 @@
 
 set -uvex -o pipefail
 
+BUILD_TARGET=${1:-""}
+
 cd $(dirname ${BASH_SOURCE[0]})/../
 
 which typos || cargo install typos-cli
 
 BUILD_COMMON="--locked --release"
+if [ x"${BUILD_TARGET}" != x"" ]; then
+    BUILD_COMMON="${BUILD_COMMON} --target ${BUILD_TARGET}"
+fi
 
 cargo clippy ${BUILD_COMMON} --all-targets --all-features -- -D warnings -D clippy::pedantic -A clippy::missing_errors_doc
 cargo clippy ${BUILD_COMMON} --tests --all-targets --all-features -- -D warnings
