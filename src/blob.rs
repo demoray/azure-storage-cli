e832708885321164e2514d2a4ec7ee9a426081a3
Brian Caswell <bcaswell@microsoft.com>
2024-01-03 18:21:34 -0500

    add support for generating container & blob SAS URLs

--- src/blob.rs
+++ src/blob.rs
@@ -1,12 +1,13 @@
 use crate::{
     args,
-    utils::{parse_key_val, round_up, to_metadata, to_tags},
+    utils::{parse_key_val, parse_time, round_up, to_metadata, to_tags, Protocol, TimeFormat},
 };
 use azure_core::{
     error::{Error, ErrorKind},
     request_options::{IfTags, LeaseId},
     tokio::fs::FileStreamBuilder,
 };
+use azure_storage::shared_access_signature::{service_sas::BlobSasPermissions, SasProtocol};
 use azure_storage_blobs::prelude::{
     AccessTier, BA512Range, BlobBlockType, BlobClient, BlobContentDisposition, BlobContentEncoding,
     BlobContentLanguage, BlobContentType, BlockList, DeleteSnapshotsMethod,
@@ -149,6 +150,51 @@ pub enum BlobSubCommands {
         sequence_number: Option<u64>,
         #[clap(long)]
         upload_block_size: Option<usize>,
+    },
+    /// Generate a SAS URL for the Blob
+    GenerateSas {
+        /// Expiration
+        expiry: String,
+        /// Start time
+        #[clap(long)]
+        start: Option<String>,
+        /// Format used for the start and expiry times
+        #[clap(long, value_enum, default_value_t = TimeFormat::Offset)]
+        time_format: TimeFormat,
+
+        #[clap(long)]
+        ip: Option<String>,
+        #[clap(long)]
+        identifier: Option<String>,
+        #[clap(long)]
+        protocol: Option<Protocol>,
+
+        #[clap(long)]
+        read: bool,
+        #[clap(long)]
+        add: bool,
+        #[clap(long)]
+        create: bool,
+        #[clap(long)]
+        write: bool,
+        #[clap(long)]
+        delete: bool,
+        #[clap(long)]
+        delete_version: bool,
+        #[clap(long)]
+        list: bool,
+        #[clap(long)]
+        tags: bool,
+        #[clap(long, name = "move")]
+        move_: bool,
+        #[clap(long)]
+        execute: bool,
+        #[clap(long)]
+        ownership: bool,
+        #[clap(long)]
+        permissions: bool,
+        #[clap(long)]
+        permanent_delete: bool,
     },
 }
 
@@ -370,6 +416,58 @@ pub async fn blob_commands(
                 let response = builder.await?;
                 debug!("{response:#?}");
             }
+        }
+        BlobSubCommands::GenerateSas {
+            expiry,
+            start,
+            time_format,
+            ip,
+            identifier,
+            protocol,
+            read,
+            add,
+            create,
+            write,
+            delete,
+            delete_version,
+            list,
+            tags,
+            move_,
+            execute,
+            ownership,
+            permissions,
+            permanent_delete,
+        } => {
+            let expiry = parse_time(&expiry, time_format)?;
+            let start = start.map(|s| parse_time(&s, time_format)).transpose()?;
+
+            let permissions = BlobSasPermissions {
+                read,
+                add,
+                create,
+                write,
+                delete,
+                delete_version,
+                permanent_delete,
+                list,
+                tags,
+                move_,
+                execute,
+                ownership,
+                permissions,
+            };
+            let mut builder = blob_client
+                .shared_access_signature(permissions, expiry)
+                .await?;
+            let protocol = protocol.map(|p| match p {
+                Protocol::Https => SasProtocol::Https,
+                Protocol::HttpHttps => SasProtocol::HttpHttps,
+            });
+
+            args!(builder, ip, identifier, protocol, start);
+
+            let url = blob_client.generate_signed_blob_url(&builder)?;
+            println!("{url}");
         }
     }
     Ok(())
