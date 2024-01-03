064d37ba25ef8cc0ef9a8f424841bc07c3528ca5
Brian Caswell <bcaswell@gmail.com>
2024-01-03 18:53:57 -0500

    use custom build profile on releases (#52)

--- eng/build.sh
+++ eng/build.sh
@@ -3,12 +3,13 @@
 set -uvex -o pipefail
 
 BUILD_TARGET=${1:-""}
+BUILD_PROFILE=${2:-"release"}
 
 cd $(dirname ${BASH_SOURCE[0]})/../
 
 which typos || cargo install typos-cli
 
-BUILD_COMMON="--locked --release"
+BUILD_COMMON="--locked --profile ${BUILD_PROFILE}"
 if [ x"${BUILD_TARGET}" != x"" ]; then
     BUILD_COMMON="${BUILD_COMMON} --target ${BUILD_TARGET}"
 fi
